2cdfb38984ec244962a610690b03e2cab08c4d0b
igneimo <ingnimo@outlook.fr>
2022-08-02 17:31:30 +0200

    register set role user

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -22,6 +22,7 @@ class RegistrationController extends AbstractController
         $user = new Participant();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
+        $user->setRoles(["ROLE_USER"]);
 
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
