4d98f9051b24f2ef8f493623f598613e0c6d4050
Salim <laimeche160@gmail.com>
2022-08-10 23:35:48 +0200

    maj sortie.show.twig.html
    ajout des pseudo des participants a une sortie, lien cliquable.
    
    ajout pseudo utilisateur dans en-tete

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,25 +2,18 @@
 
 namespace App\Controller;
 
-use App\Entity\Etat;
+
 use App\Entity\Participant;
 use App\Entity\Sortie;
 use App\Form\SortieType;
-use App\Repository\EtatRepository;
 use App\Repository\ParticipantRepository;
 use App\Repository\SortieRepository;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
-use http\Message;
-use mysql_xdevapi\Exception;
-use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
 
 #[Route('/sortie')]
 class SortieController extends AbstractController
